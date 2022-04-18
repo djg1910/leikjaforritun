a200b50985c6eda567982c1af67c8dfdb3144835
Darri Jökull Gunnarsson <70219292+djg1910@users.noreply.github.com>
2022-04-18 20:59:53 +0000

    Update enemy_master.cs

--- Scripts/enemy_master.cs
+++ Scripts/enemy_master.cs
@@ -7,11 +7,11 @@ public class enemy_master : MonoBehaviour
 {
 
     public GameObject Player;
-    public float Distance;
+    public float Distance; //finnur lengd frá player
 
-    public bool isAngered;
+    public bool isAngered; //gerir óvin reiðan og hann eltir þig
 
-    public NavMeshAgent _agent;
+    public NavMeshAgent _agent; //lætur óvin geta elt þig
 
     void Start()
     {
@@ -20,24 +20,24 @@ public class enemy_master : MonoBehaviour
 
     void update()
     {
-        Distance = Vector3.Distance(Player.transform.position, this.transform.position);
+        Distance = Vector3.Distance(Player.transform.position, this.transform.position);  //finnur lengd frá player
 
         if(Distance <=5)
         {
-            isAngered = true;
+            isAngered = true;  //gerir óvin reiðan
         }
         if(Distance > 5f)
         {
-            isAngered = false;
+            isAngered = false;  //lætur óvin hætta að vera reiður
         }
 
         if(isAngered)
         {
-            _agent.SetDestination(Player.transform.position);
+            _agent.SetDestination(Player.transform.position);  //ef óvinur er reiður eltir hann player
         }
         if(!isAngered)
         {
-            _agent.isStopped = true;
+            _agent.isStopped = true;  //ef hann er ekki reiður stoppar hann
         }
     }
 }
